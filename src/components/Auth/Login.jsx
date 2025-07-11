fa5d98df6d2fd94d216743e5a25039e4f32f531d
Saurabh379K <saurabhsemwal2003@gmail.com>
2025-07-11 23:37:44 +0530

    completed login page

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     const submitHandler = (e) => {
         e.preventDefault()
         console.log("Form Submitted");
-        
+        console.log("Email:", email);
+        console.log("Password:", password);
+
+        setEmail('')
+        setPassword('')
     }
 
 
@@ -16,8 +24,18 @@ const Login = () => {
                 submitHandler(e)
             }}
             className='flex flex-col items-center justify-center'>
-                <input required className='outline-none border-2 border-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-gray-400' type="email" placeholder='Enter your email' />
-                <input required className='outline-none border-2 border-emerald-600 text-xl py-3 px-5 rounded-full mt-3 placeholder:text-gray-400' type="password" placeholder='Enter your password' />
+                <input 
+                value={email}
+                onChange={(e) => {
+                    setEmail(e.target.value)
+                }}
+                required autoComplete='username' className='outline-none border-2 border-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-gray-400' type="email" placeholder='Enter your email' />
+                <input 
+                value={password}
+                onChange={(e) => {
+                    setPassword(e.target.value)
+                }}
+                required autoComplete='current-password' className='outline-none border-2 border-emerald-600 text-xl py-3 px-5 rounded-full mt-3 placeholder:text-gray-400' type="password" placeholder='Enter your password' />
                 <button className='mt-7 outline-null border-none hover:bg-emerald-700 font-semibold bg-emerald-600 text-lg py-2 px-8 w-full rounded-full placeholder:text-white'>Log in</button>
             </form>
         </div>
