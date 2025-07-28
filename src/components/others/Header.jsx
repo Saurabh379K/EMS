fd9d1b57e35dced62bbae77e0c2ac8a1225d25a2
Saurabh379K <saurabhsemwal2003@gmail.com>
2025-07-28 22:07:36 +0530

    created tasks

--- src/components/others/Header.jsx
+++ src/components/others/Header.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import { setLocalStorage } from '../../utils/localStorage'
 
-const Header = () => {
+const Header = (props) => {
 
   // const [username, setusername] = useState('')
   // if(!data) {
@@ -13,7 +13,8 @@ const Header = () => {
 
   const logOutUser = () => {
     localStorage.setItem('loggedInUser','')
-    window.location.reload()
+    props.changeUser('')
+    // window.location.reload()
   }
 
   return (
